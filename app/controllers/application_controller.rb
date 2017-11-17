bd04d3b0c10961d7fa496c22c7189eea4545c1ea
Matt Miller <matthew.miller-4@mnsu.edu>
2017-11-17 03:40:27 +0000

    added some time stuff thats commented out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,9 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+  
+  def display_time_widget
+    @timeWidget ||= TimeWidget(1, 1)
+    @timeWidget.getTime
+  end
 end
