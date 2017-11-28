68c8431850c6c939903261148527bfb33405a31e
Pujit <pujit.koirala@mnsu.edu>
2017-11-28 14:29:03 +0000

    movie model is created as there are seed datas related to the Moview

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107193127) do
+ActiveRecord::Schema.define(version: 20171128142719) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "title"
+    t.string   "rating"
+    t.date     "release_date"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
